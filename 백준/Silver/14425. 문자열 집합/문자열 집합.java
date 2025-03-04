22e928b376778e7275d35b9eb9835b294552d452
Hyeyeon Kim <99088509+iamhyeyeon@users.noreply.github.com>
2025-03-04 22:35:19 +0900

    [Silver IV] Title: 문자열 집합, Time: 360 ms, Memory: 31232 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.java
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.java
@@ -6,19 +6,26 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer command;
+
+        command = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(command.nextToken());
+        int M = Integer.parseInt(command.nextToken());
+
         Set<String> set = new HashSet<>();
+        int cnt = 0;
 
         for (int i=0; i<N; i++) {
             set.add(br.readLine());
         }
-        int cnt = 0;
+
         for (int i=0; i<M; i++) {
-           if (set.contains(br.readLine())) cnt ++;
+            String s = br.readLine();
+            if (set.contains(s)) {
+                cnt++;
+            }
         }
 
         System.out.println(cnt);
