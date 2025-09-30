62af7a2b7b658060f9e4dc478826c233c0ecda7b
Hyeyeon Kim <99088509+iamhyeyeon@users.noreply.github.com>
2025-10-01 00:03:34 +0900

    [level 2] Title: 의상, Time: 1.66 ms, Memory: 81.1 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -2,18 +2,19 @@ import java.util.*;
 
 class Solution {
     public int solution(String[][] clothes) {
-        int answer = 1;
+        int answer = 0;
         Map<String, Integer> clotheMap = new HashMap<>();
         
         // 종류, 이름 hashMap 만들기 --> 이름은 알 필요가없지
-        Arrays.stream(clothes)
+        Arrays.stream()
             .forEach(clothe -> clotheMap
-            .merge(clothe[1], 1, Integer::sum));
-                     
-        for (Integer type : clotheMap.values()) {
-            answer *= type+1;
+            .getOrDefault(clothe[1], 0)
+        
+        
+            // System.out.println(clothe[0]);
+            
         }
         
-        return answer-1;
+        return answer;
     }
 }
